fa98f8d17b1edaed63677085ab842cfdeba71bd6
AagmanSuri <63902687+AagmanSuri@users.noreply.github.com>
2021-07-08 20:10:26 +0530

    update

--- src/components/CoustomerData.js
+++ src/components/CoustomerData.js
@@ -2,11 +2,59 @@ import React,{useState} from 'react';
 
 
 const CoustomerData = () => {  
-    return(
-<div style={{textAlign:'center'}} >
+
+  
+  //input which we are taking 
+  const [inputValue,setInputValue] = useState()
+  
+  const baldict = [100,1,2];
+  
+  const inputHandeler = (event)=>{
+    console.log(event.target.value);
+    // console.log(bal1);
+    setInputValue(Number(event.target.value))
+  }
+
+  const [dropdownValue1,setdropdownVAlue1]=useState();
+
+  const optionHandel1=(event)=>{
+    setdropdownVAlue1(event.target.value)
+  }
+
+  const [dropdownValue2,setdropdownVAlue2]=useState();
+
+  const optionHandel2=(event)=>{
+    setdropdownVAlue2(event.target.value)
+  }
+  const btnHandeler = () =>{
+     console.log(dropdownValue1,dropdownValue2)
+     console.log('dpvalue',{dropdownValue1})
+     
+  }
+    return (
+  <div>
+    
+    <select onChange={optionHandel1}>
+      <option value="a">jhon</option>
+      <option value="a">marry</option>
+      <option value="a">july</option>
+    </select>
+    <select onChange={optionHandel2}>
+      <option value="{bal1}">jhon</option>
+      <option value="{bal2}">marry</option>
+      <option value="{bal3}">july</option>
+    </select>
+    
+    <p>first {dropdownValue1}</p>
+    <p>second {dropdownValue2}</p>
+    
+  
+    
+    <h2>Balance</h2>
+    
     {/* table */}
-    <input  type='Number'/>
-    <button >transfer</button>
+    <input onChange={inputHandeler} type='Number'/>
+    <button onClick={btnHandeler} >transfer</button>
     <table>
       <thead>
         <tr>
@@ -19,17 +67,17 @@ const CoustomerData = () => {
         <tr>
           <td>John</td>
           <td>Doe</td>
-          <td>1000</td>
+          <td>{baldict[0]}</td>
         </tr>
         <tr>
           <td>Mary</td>
           <td>Moe</td>
-          <td>123</td>
+          <td>{baldict[1]}</td>
         </tr>
         <tr>
           <td>July</td>
           <td>Dooley</td>
-          <td>1400</td>
+          <td>{baldict[2]}</td>
         </tr>
       </tbody>
     </table>
