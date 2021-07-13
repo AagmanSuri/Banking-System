970ad42d1e540ba23f0b1fd9060db0351e7a808b
AagmanSuri <63902687+AagmanSuri@users.noreply.github.com>
2021-07-13 18:49:55 +0530

    up

--- src/components/Transcations/Transcation.js
+++ src/components/Transcations/Transcation.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Transcation(props) {
     return (
         <div>
-            <h1>Transcation</h1>
+            <h1>Last Transcation</h1>
             <h2>Money Tranfered to {props.name[props.indexofOption1]}</h2>
             <p>updated balance{props.balance[props.indexofOption1]}</p>
             <h2>Money Tranfered from {props.name[props.indexofOption2]}</h2>
